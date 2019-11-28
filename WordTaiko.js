19739a605eadccb0ffa3b336283a2f00b6a7e6b0
Leon <ChinL@outlook.co.nz>
2019-11-28 17:12:04 +1300

    Added generic styling to text input and put query into method

--- WordTaiko.js
+++ WordTaiko.js
@@ -3,12 +3,20 @@ const apiKey = "P7AP6168";
 const http = new XMLHttpRequest();
 const url = "https://random-word-api.herokuapp.com/word?key=" + apiKey + "&number="+wordCount;
 
+function getWords(){
+    http.open("GET", url);
+    http.send();
+    http.onreadystatechange = (e) => {
+        console.log(http.responseText);
+        wordList = http.responseText;
+    }
+}
+
+var wordList;
+getWords();
+
 $(document).ready(function () {
     $("#start").click(function () {
-        http.open("GET", url);
-        http.send();
-        http.onreadystatechange = (e) => {
-            console.log(http.responseText);
-        }
+        wordList = getWords();
     });
 });
